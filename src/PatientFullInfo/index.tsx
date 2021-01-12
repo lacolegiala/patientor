716ca0132e4c06137df69d10757b378139991ab7
Sofia Riekkola <sofia.riekkola@gmail.com>
2021-01-12 17:15:28 +0200

    Render id on individual patient component

--- src/PatientFullInfo/index.tsx
+++ src/PatientFullInfo/index.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 const PatientFullInfo: React.FC = () => {
+
+  const { id } = useParams<{ id: string }>();
   return (
     <div>
       <h2>Name</h2>
+      <p>{id}</p>
       <p>ssn</p>
       <p>occupation</p>
     </div>
