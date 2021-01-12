c93654c908efb5489c9fd0df28dad03334d59129
Sofia Riekkola <sofia.riekkola@gmail.com>
2021-01-12 16:05:05 +0200

    Add base for individual patient component

---
+++ src/PatientFullInfo/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const PatientFullInfo: React.FC = () => {
+  return (
+    <div>
+      <h2>Name</h2>
+      <p>ssn</p>
+      <p>occupation</p>
+    </div>
+  );
+};
+
+export default PatientFullInfo;
