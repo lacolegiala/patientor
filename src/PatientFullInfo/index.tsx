8b6ae46905c1e7768587eff17ae2da894f1e3856
Sofia Riekkola <sofia.riekkola@gmail.com>
2021-01-13 10:24:47 +0200

    Fetch individual patient data

--- src/PatientFullInfo/index.tsx
+++ src/PatientFullInfo/index.tsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import axios from "axios";
 import { useParams } from 'react-router-dom';
+import { Patient } from '../types';
+import { apiBaseUrl } from '../constants';
 
 const PatientFullInfo: React.FC = () => {
 
   const { id } = useParams<{ id: string }>();
+
+  React.useEffect(() => {
+    axios.get<Patient>(`${apiBaseUrl}/patients/${id}`);
+
+    const fetchPatientData = async () => {
+      try {
+        const {data} = await axios.get<Patient>(
+          `${apiBaseUrl}/patients/${id}`
+        );
+        console.log(data);
+      }
+      catch (error) {
+        console.log(error);
+      }
+    };
+    fetchPatientData();
+  });
   return (
     <div>
       <h2>Name</h2>
